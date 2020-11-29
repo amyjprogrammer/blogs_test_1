5c2e8fc45b57bd36b6d8690b12a34dbe7eaddfd1
amyjprogrammer <70500009+amyjprogrammer@users.noreply.github.com>
2020-11-28 22:40:19 -0500

    Set DEBUG for Heroku

--- Blog/settings.py
+++ Blog/settings.py
@@ -130,5 +130,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 #Heroku settings
+import os
 import django_heroku
 django_heroku.settings(locals())
+
+if os.environ.get('DEBUG') == 'TRUE':
+    DEBUG = True
+if os.environ.get("DEBUG") == 'FALSE':
+    DEBUG = False
